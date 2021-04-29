a9393cd1a0d5e5732a81ee1c23aec613a2ad3a6c
David Liao <liao.canada@gmail.com>
2021-04-29 14:41:37 -0400

    Add navbar, use Bootstrap 3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# README
-A description of the project.
+# Welcome!
+This is my personal website, hosted at [davidliao.ca](davidliao.ca).
+
+It includes some basic information about myself and some of my projects.
+
+It was previously a Spring/Thymeleaf web application, but in an effort to reduce costs associated with running a server, I've converted it into a static React app. (So, most of the stuff is still in HTML rather than React components!)
--- src/views/shared/components/Header.js
+++ src/views/shared/components/Header.js
@@ -2,7 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <h1>Header</h1>
+  <>
+    <nav className="navbar">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar-collapse"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <Link className="navbar-brand" to="/">Home</Link>
+          {/* <a className="navbar-brand" th:href="@{/home}">Home</a> */}
+        </div>
+
+        <div className="collapse navbar-collapse" id="navbar-collapse">
+          <ul className="nav navbar-nav navbar-right nav">
+            <li><Link to="/resume">Resume</Link></li>
+            <li><Link to="/achievements">Achievements</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div style={{"margin-top": "62px"}}></div>
+  </>
 );
 
 export default Header;
