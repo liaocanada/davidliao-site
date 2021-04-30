a5afad91412c67e7e97193324e04fc56be742172
David Liao <liao.canada@gmail.com>
2021-04-30 09:09:50 -0400

    Change the page's browser title on each page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './css/bootstrap.css';
 import './css/index.css';
 import App from './views/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
+  useLocation,
 } from 'react-router-dom';
+import capitalize from '../helpers/capitalize';
 
 // Pages
 import Home from './Home';
@@ -13,17 +14,31 @@ import Contact from './Contact';
 import Projects from './Projects';
 import Resume from './Resume';
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path={["/", "/home"]}><Home /></Route>
-      <Route exact path="/about"><About /></Route>
-      <Route exact path="/achievements"><Achievements /></Route>
-      <Route exact path="/contact"><Contact /></Route>
-      <Route exact path="/projects"><Projects /></Route>
-      <Route exact path="/resume"><Resume /></Route>
-    </Switch>
-  </Router>
-);
+const updatePageTitle = pageName =>
+  document.title = !!pageName ? `${capitalize(pageName)} | David Liao` : "David Liao";
+
+const updateHitCount = pageName => {
+  // TODO
+};
+
+const App = () => {
+  const location = useLocation();
+  const page = location.pathname.split("/")[1];
+  useEffect(() => {
+    updatePageTitle(page);
+    updateHitCount(page);
+  }, [page]);
+
+  return (
+      <Switch>
+        <Route exact path={["/", "/home"]}><Home /></Route>
+        <Route exact path="/about"><About /></Route>
+        <Route exact path="/achievements"><Achievements /></Route>
+        <Route exact path="/contact"><Contact /></Route>
+        <Route exact path="/projects"><Projects /></Route>
+        <Route exact path="/resume"><Resume /></Route>
+      </Switch>
+  )
+};
 
 export default App;
