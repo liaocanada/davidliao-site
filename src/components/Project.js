edd9b444da10cd8aedccf8bacee535f95ddcd125
David Liao <liao.canada@gmail.com>
2021-05-21 14:34:36 -0400

    Add subheadings to project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 import allSkillsData from '../data/skills.json';
 
-const Project = ({ name, description, image, skills, url }) => {
-
+const Project = ({ name, description, image, skills, url, companyName, companyUrl }) => {
   return (
     <div className="col-md-4 thumbnail-wrapper">
       <div className="thumbnail">
         <img src={image} alt="Project Image" />
 
         <div className="caption">
-          <a href={url}><h3>{name}</h3></a>
+          {url ?
+            <h3><a href={url}>
+              {name}
+              {url && <>&nbsp;&nbsp;<FontAwesomeIcon icon="external-link-alt" size="xs" /></>}
+            </a></h3> :
+            <h3 className="text-primary">{name}</h3>
+          }
+          {companyName && companyUrl &&
+            <a href={companyUrl}>
+              <h5><i>{companyName}</i></h5>
+            </a>
+          }
+          {companyName && !companyUrl &&
+              <h5><i>{companyName}</i></h5>
+          }
 
           <ReactMarkdown>{description}</ReactMarkdown>
 
-          <p style={{wordBreak: "break-all"}}>
+          <p style={{ wordBreak: "break-all" }}>
             {skills.map(skill => (
               <a key={skill} href="#">
                 <span id="skill-tag" className={`label ${allSkillsData[skill].bootstrapClassName}`}>{skill}</span>
